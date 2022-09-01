d94842dc32dbaf1c0658019d69dddf263716bb78
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2022-09-01 23:16:21 +1200

    get token

--- client/apiClient.js
+++ client/apiClient.js
@@ -7,3 +7,14 @@ export function getWelcome() {
   return request.get(`${serverURL}/welcome`).then((response) => response.body)
 }
 // ***   ***   ***
+
+export function getToken() {
+  return request.get(`${serverURL}/spotify`).then((res) => res.body)
+}
+
+export function getSpotifyArtist(token) {
+  return request
+    .post(`${serverURL}/spotify/artist`)
+    .send({ token })
+    .then((res) => res.body)
+}
