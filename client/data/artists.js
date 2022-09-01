ea6c0522f8cf2a6aaa8e77fd2f4a3101c7020a3a
paul-luo-22 <108055124+paul-luo-22@users.noreply.github.com>
2022-09-01 16:29:35 +1200

    Create list of artists

---
+++ client/data/artists.js
@@ -0,0 +1,82 @@
+export default [
+  {
+    name: "Ed Sheeran",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Ariana Grande",
+	  id: "66CXWjxzNUsdJxJ2JdwvnR"
+  },
+  {
+    name: "Billie Eilish",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Drake",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Justin Bieber",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Eminem",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Taylor Swift",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Arijit Singh",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Bad Bunny",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "BTS",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Rihanna",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "The Weeknd",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Queen",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Bruno Mars",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Imagine Dragons",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Adele",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Shawn Mendes",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "XXXTENTACION",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Post Malone",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  },
+  {
+    name: "Coldplay",
+	  id: "6eUKZXaKkcviH0Ku9w2n3V"
+  }
+]
