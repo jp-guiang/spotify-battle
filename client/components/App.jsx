bad4e16fe5a297229015cde7c3c55bfaf408fbaa
Aaron <95986810+Aaront028@users.noreply.github.com>
2022-09-02 13:35:28 +1200

    form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,9 +6,20 @@ import artists from '../data/artists'
 function App() {
   const [token, setToken] = useState('')
   const [artist, setArtist] = useState('')
-  const [allArtists, setAllArtists] = useState([])
+  const [allArtistData, setAllArtistData] = useState([])
   const [artistQuery, setArtistQuery] = useState('')
   const [winner, setWinner] = useState(0)
+  const [playerSub, setSub] = useState(
+    '0TnOYISbd1XYRBk9myaseg,7dGJo4pcD2V6oG8kP0tJRR,06HL4z0CvFAxyc27GXpf02'
+  )
+  const [playerAnswers, setAnswers] = useState([])
+
+  const initialFormData = {
+    playerOne: '',
+    playerTwo: '',
+  }
+
+  const [form, setForm] = useState(initialFormData)
 
   // get token on loading
   useEffect(() => {
@@ -21,8 +32,8 @@ function App() {
         return setArtist(artistObj.name)
       })
       .then(() => {
-        const test = getAllArtistsQuery(artists)
-        return setArtistQuery(test)
+        const artistIds = getAllArtistsQuery(artists)
+        return setArtistQuery(artistIds)
       })
       .catch((err) => {
         console.error(err.message)
@@ -52,7 +63,7 @@ function App() {
   function getAllArtists() {
     spotifyAllArtists(token, artistQuery)
       .then(({ artists }) => {
-        setAllArtists(artists)
+        setAllArtistData(artists)
       })
       .catch((err) => {
         console.error(err.message)
@@ -60,13 +71,22 @@ function App() {
   }
 
   function handleSubmit(e) {
+    console.log(form)
+    let answersId = ''
+
+    answersId = answersId.concat(form.playerOne, ',', form.playerTwo)
+    console.log(answersId)
+    setSub(answersId)
+
     e.preventDefault()
     //only pass in the two artists, arist query can be diff variable
-    spotifyAllArtists(token, artistQuery)
+    spotifyAllArtists(token, playerSub)
       .then(({ artists }) => {
-        artists.map((element) => console.log(element.images))
+        setAnswers(artists)
 
-        setAllArtists(artists)
+        // artists.map((element) => console.log(element.images))
+
+        // setAllArtistData(artists)
         // if (artists[0].popularity > artists[1].popularity) {
         //   setWinner(1)
         // } else if (artists[1].popularity > artists[0].popularity) {
@@ -78,12 +98,22 @@ function App() {
       })
   }
 
+  function handleChange(e) {
+    const { name, value } = e.target
+
+    const newForm = {
+      ...form,
+      [name]: value,
+    }
+    setForm(newForm)
+  }
+
   return (
     <>
       {/* <h1>{welcomeStatement}</h1> */}
       <h1>{'Spotiguys Project'}</h1>
 
-      {allArtists.map((element) => {
+      {allArtistData.map((element) => {
         return (
           <p key={element.name}>
             {element.name} {element.id}
@@ -91,7 +121,7 @@ function App() {
         )
       })}
 
-      {allArtists.map((element) => {
+      {playerAnswers.map((element) => {
         return (
           <div key={element.uri}>
             <p>Artist Name: {element.name}</p>
@@ -101,9 +131,26 @@ function App() {
         )
       })}
       <form onSubmit={handleSubmit}>
-        <input></input>
-        <input type="submit" />
-        <input></input>
+        <label htmlFor="playerOne">
+          Player 1:
+          <input
+            id="playerOne"
+            onChange={handleChange}
+            name="playerOne"
+            value={form.playerOne}
+          ></input>
+        </label>
+
+        <label htmlFor="playerTwo">
+          Player 2:
+          <input
+            id="playerTwo"
+            onChange={handleChange}
+            name="playerTwo"
+            value={form.playerTwo}
+          ></input>
+          <input type="submit" />
+        </label>
       </form>
     </>
   )
